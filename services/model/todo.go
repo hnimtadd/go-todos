35d4aeecb3b49bc9ebfa0f3d41c2685b7eedf40e
hnimtadd <minhdat15012002@gmail.com>
2023-06-07 12:40:50 +0700

    main: add enitiy

---
+++ services/model/todo.go
@@ -0,0 +1,11 @@
+package model
+
+import "time"
+
+type Todo struct {
+	Id        string `gorm:"primary_key"`
+	Content   string
+	CreatedAt time.Time
+	CreatedBy string
+	User      User `gorm:"foreignKey:CreatedBy"`
+}
