93a34b0c96c6b2a88e0f54b163142d221abc2e95
hnimtadd <minhdat15012002@gmail.com>
2023-06-08 13:24:16 +0700

    feature: add request userid key

--- services/auth/repository.go
+++ services/auth/repository.go
@@ -5,6 +5,8 @@ import (
 	"context"
 )
 
+const CtxUserKey = "userId"
+
 type UserRepository interface {
 	CreateUser(ctx context.Context, user *model.User) error
 	GetUserByUsername(ctx context.Context, username string) (*model.User, error)
