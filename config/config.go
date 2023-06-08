185ac874075872b9cce15932bc3f66498d8b38a2
hnimtadd <minhdat15012002@gmail.com>
2023-06-08 16:18:44 +0700

    base: add config struct

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
+)
+
+type Configuration struct {
+	Port                  string `env:"PORT" envDefault:"8080"`
+	HashSalt              string `env:"HASH_SAL,required"`
+	SigningKey            string `env:"SIGNING_KEY,required"`
+	TokenTTL              int64  `env:"TOKEN_TTL,required"`
+	JwtSecret             string `env:"JWT_SECRET,required"`
+	DatabaseConnectionURL string `env:"CONNECTION_URL,required"`
+}
+
+func NewConfig(files ...string) *Configuration {
+	err := godotenv.Load(files...)
+	if err != nil {
+		log.Printf("No .env file could be found %q\n", files)
+	}
+	cfg := Configuration{}
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("%v\n", err.Error())
+	}
+	return &cfg
+}
